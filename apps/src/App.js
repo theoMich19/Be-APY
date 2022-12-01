d17f5418dd217dd641063175f1dec6df9ac195f2
Valentin DEDET <valentin.dedet@hotmail.fr>
2022-12-01 23:50:38 +0100

    Add principal routes

--- apps/src/App.js
+++ apps/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import './components/HomePage.js';
+import './components/DashBoardPage.js';
+import './components/InformPage.js';
+import './components/HighScoresPage.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world !</h1>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/InformPage/" element={<InformPage />} />
+            <Route path="/DashBoardPage/" element={<DashBoardPage />} />
+            <Route path="/HighScoresPage/" element={<HighScoresPage />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
