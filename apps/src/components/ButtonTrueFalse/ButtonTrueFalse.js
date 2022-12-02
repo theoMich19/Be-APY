761ccc65b65bdd7db546d6e2831491af674f3690
Valentin DEDET <valentin.dedet@hotmail.fr>
2022-12-02 04:00:22 +0100

    Add trueFalse interface

---
+++ apps/src/components/ButtonTrueFalse/ButtonTrueFalse.js
@@ -0,0 +1,14 @@
+import "./ButtonTrueFalse.css"
+
+const ButtonTrueFalse = () => {
+    return (
+       <div>
+           <button className="buttonTrueFalse">Vrai</button>
+           <button className="buttonTrueFalse">Faux</button>
+       </div>
+    )
+   
+   }
+   
+   
+   export default ButtonTrueFalse
--- apps/src/components/QuestionPage/QuestionPage.js
+++ apps/src/components/QuestionPage/QuestionPage.js
@@ -5,15 +5,20 @@ import ButtonAbandon from "../ButtonAbandon/ButtonAbandon";
 import ButtonValider from "../ButtonValider/ButtonValider";
 import ButtonPass from "../ButtonPass/ButtonPass";
 import './questionPage.css'
+import ButtonReponse from "../ButtonReponse/ButtonReponse";
+import ButtonTrueFalse from "../ButtonTrueFalse/ButtonTrueFalse";
 
 export default function QuestionPage() {
-    const [typeQuestion, setTypeQuestion] = useState("input");
+    const [typeQuestion, setTypeQuestion] = useState("trueFalse");
 
     return (
             <div className="questionPage">
                 <p className="time">02:45</p>
                 <CardQuestion></CardQuestion>
-                {typeQuestion === "input" ? <TextImputReponse></TextImputReponse> : <view></view>}
+                {typeQuestion === "input" ? <TextImputReponse></TextImputReponse> 
+                : typeQuestion === "multiple" ? <ButtonReponse></ButtonReponse> 
+                : typeQuestion === "trueFalse" ? <ButtonTrueFalse></ButtonTrueFalse> 
+                : <view></view>}
                 <ButtonPass></ButtonPass>
                 <div className="buttons">
                     <ButtonAbandon></ButtonAbandon>
