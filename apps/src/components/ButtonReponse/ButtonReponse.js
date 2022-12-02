8ead6453f85b26f1ee1abd9d14a4f3282dde7815
Clement Lantiat <clement.lantiat@gmail.com>
2022-12-02 03:00:32 +0100

    bouttons reponse

---
+++ apps/src/components/ButtonReponse/ButtonReponse.js
@@ -0,0 +1,13 @@
+import "./ButtonReponse.css"
+
+const ButtonReponse = () => {
+    return (
+       <div>
+           <button className="buttonQuestionReponse">Reponse 1</button>
+       </div>
+    )
+   
+   }
+   
+   
+   export default ButtonReponse
--- apps/src/components/HomePage.js
+++ apps/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import ButtonAbandon from "./ButtonAbandon/ButtonAbandon"
 import ButtonPass from "./ButtonPass/ButtonPass"
+import ButtonReponse from "./ButtonReponse/ButtonReponse"
 import ButtonValider from "./ButtonValider/ButtonValider"
 import Card from "./CardQuestion/CardQuestion"
 import TextImputReponse from "./TextImputReponse/TextImputReponse"
@@ -10,6 +11,7 @@ export const HomePage = () => {
            <ButtonPass />
            <ButtonAbandon />
            <ButtonValider />
+           <ButtonReponse />
        </div>
     )
    
