d73c0f4f55b6b3e3d994ec802e2aee1a109e608f
Clement Lantiat <clement.lantiat@gmail.com>
2022-12-02 03:26:28 +0100

    buttons reponse

--- apps/src/components/ButtonReponse/ButtonReponse.js
+++ apps/src/components/ButtonReponse/ButtonReponse.js
@@ -4,6 +4,10 @@ const ButtonReponse = () => {
     return (
        <div>
            <button className="buttonQuestionReponse">Reponse 1</button>
+           <button className="buttonQuestionReponse">Reponse 2</button>
+           <br></br>
+           <button className="buttonQuestionReponse">Reponse 3</button>
+           <button className="buttonQuestionReponse">Reponse 4</button>
        </div>
     )
    
--- apps/src/components/HomePage.js
+++ apps/src/components/HomePage.js
@@ -1,16 +1,13 @@
 import ButtonAbandon from "./ButtonAbandon/ButtonAbandon"
 import ButtonPass from "./ButtonPass/ButtonPass"
-import ButtonReponse from "./ButtonReponse/ButtonReponse"
 import ButtonValider from "./ButtonValider/ButtonValider"
+import ButtonReponse from "./ButtonReponse/ButtonReponse"
 import Card from "./CardQuestion/CardQuestion"
 import TextImputReponse from "./TextImputReponse/TextImputReponse"
 
 export const HomePage = () => {
     return (
        <div>
-           <ButtonPass />
-           <ButtonAbandon />
-           <ButtonValider />
            <ButtonReponse />
        </div>
     )
