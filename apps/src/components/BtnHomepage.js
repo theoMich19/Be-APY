fc3fa6438b5037d6799e13780d2d7b03e81528a7
ugomrtt <mariotto.ugo@gmail.com>
2022-12-02 02:28:15 +0100

    push branch

---
+++ apps/src/components/BtnHomepage.js
@@ -0,0 +1,11 @@
+const BtnHomepage = () => {
+    return (
+       <a href="google.com" className="btnHomePage">
+           <p>JOUER</p>
+       </a>
+    )
+   
+   }
+   
+   
+   export default BtnHomepage
--- apps/src/routes/HomePage.js
+++ apps/src/routes/HomePage.js
@@ -1,7 +1,14 @@
+import BtnHomepage from '../components/BtnHomepage'
+import homePage from './homePage.css'
+
 export const HomePage = () => {
     return (
-       <div>
-           <h1>HomePage</h1>
+       <div className='screen'>
+        <h1>Be'APY</h1>
+        <p className='text'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at laoreet nulla, ut sollicitudin nulla. Vivamus eu interdum dolor. Nam eleifend mollis faucibus. Nulla id mi eu risus consectetur faucibus a et tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In ultrices velit risus, id dapibus mauris pharetra eu.
+        </p>
+           <BtnHomepage/>
        </div>
     )
    
